320be8d3d7c27f303b8f0775dbbb37a278b8862b
zayan93 <zayan_93@mail.ru>
2025-08-21 13:05:55 +0300

    i love tests

--- internal/app/handler.go
+++ internal/app/handler.go
@@ -250,7 +250,7 @@ func (h *Handler) PostOrders(res http.ResponseWriter, req *http.Request) {
 		if existingOrder.UserID == userID {
 			// Заказ уже был загружен этим пользователем
 			logger.Log.Info("Order already uploaded by this user", zap.String("orderNum", orderNum), zap.Int("userID", userID))
-			res.WriteHeader(http.StatusAccepted)
+			res.WriteHeader(http.StatusOK)
 			return
 		} else {
 			// Заказ уже был загружен другим пользователем
